30fc892840b45158538aa0be20afbfb2e6e748e5
Christopher Terrano <mterrano1@yahoo.com>
2022-01-22 00:59:37 -0600

    commit tree d899ef1c182ab2ddeaa24dba562d5a55af2327b6

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,14 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+function EyesOnMe() {
+    function handleEyes(e) {
+        console.log(e)
+    }
+
+    return (
+        <button onBlur={() => handleEyes('Hey! Eyes on me!')} onFocus={() => handleEyes('Good!')}>Eyes on me</button>
+    )
+}
+
+export default EyesOnMe;
